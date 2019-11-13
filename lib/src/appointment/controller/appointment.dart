fcc0c549d88101fb9ff58a9b919b3b793013d05e
ichibo <chibo@auxmoney.com>
2019-11-13 22:35:08 +0100

    preparing review

--- lib/src/appointment/controller/appointment.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:skg_hagen/src/appointment/view/cards.dart';
-
-class Appointment extends StatefulWidget {
-  static const String route = '/appointment';
-
-  @override
-  Cards createState() => Cards();
-}
