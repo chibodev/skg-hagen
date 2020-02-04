77155f00ef5794c96e3585486d4df18972314670
ichibo <chibo@auxmoney.com>
2020-02-04 17:24:29 +0100

    release: fixing test

--- test/unit/home/model/cardContent_test.dart
+++ test/unit/home/model/cardContent_test.dart
@@ -6,20 +6,23 @@ void main() {
   String title;
   List<String> subtitle;
   String routeName;
+  String name;
   String custom;
 
   setUpAll(() {
     title = 'title';
     subtitle = <String>['first', 'second'];
     routeName = '/route';
+    name = 'name';
     custom = 'custom';
-    subject = CardContent(title, subtitle, routeName, custom);
+    subject = CardContent(title, subtitle, routeName, name, custom);
   });
 
   test('CardContent model creates and gets properties successfully', () {
     expect(subject.title, title);
     expect(subject.subtitle, subtitle);
     expect(subject.routeName, routeName);
+    expect(subject.name, name);
     expect(subject.custom, custom);
   });
 
